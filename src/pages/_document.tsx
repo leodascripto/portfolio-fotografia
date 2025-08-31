39fda092af15615c5d63146e551833201942719c
leodascripto <leodascripto@gmail.com>
2025-08-30 21:54:06 -0300

    arrumando o erro do firebase

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+
 export default function Document() {
   return (
-    <Html lang="pt-BR">
+    <Html lang="pt-br">
       <Head>
-        {/* Fontes */}
-        <link href="https://fonts.googleapis.com/css?family=Parisienne&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet" />
-
-        {/* Ícones */}
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" />
+        {/* Google Fonts */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap"
+        />
+        {/* Font Awesome */}
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
+        />
       </Head>
       <body>
         <Main />
